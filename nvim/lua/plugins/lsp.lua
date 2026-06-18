22697c18330bad843f9a33e38e14bcc7dc402a1c
Jaefae <Jaefae@icloud.com>
2026-06-18 14:42:34 -0400

    Fix lsp clang-format error

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,60 +1,59 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = true 
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = { "clangd", "lua_ls" },
-    },
-  },
-  {
-    -- Installs non-LSP tools (formatters, debuggers) through Mason
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = {
-      ensure_installed = { "clang-format", "stylua" },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      -- The new Neovim 0.11+ way
-      -- Instead of require('lspconfig').clangd.setup({})
-      
-      -- Setup C++ (clangd)
-      vim.lsp.config('clangd', {
-        capabilities = { offsetEncoding = { "utf-16" } },
-        -- Optional: add your specific project arguments here
-        cmd = { "clangd", "--background-index" },
-      })
+	{
+		"williamboman/mason.nvim",
+		config = true,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = { "clangd", "lua_ls" },
+		},
+	},
+	{
+		-- Installs non-LSP tools (formatters, debuggers) through Mason
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_installed = { "stylua" },
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			-- The new Neovim 0.11+ way
+			-- Instead of require('lspconfig').clangd.setup({})
 
-      -- Setup Lua
-      vim.lsp.config('lua_ls', {})
+			-- Setup C++ (clangd)
+			vim.lsp.config("clangd", {
+				capabilities = { offsetEncoding = { "utf-16" } },
+				-- Optional: add your specific project arguments here
+				cmd = { "clangd", "--background-index" },
+			})
 
-      -- Start the servers for the current buffer
-      vim.lsp.enable({ 'clangd', 'lua_ls' })
+			-- Setup Lua
+			vim.lsp.config("lua_ls", {})
 
-      -- Neovim 0.11 disables inline diagnostic text by default
-      vim.diagnostic.config({ virtual_text = true })
+			-- Start the servers for the current buffer
+			vim.lsp.enable({ "clangd", "lua_ls" })
 
-      -- Global Diagnostic Mappings
-      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-      vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-      vim.api.nvim_create_autocmd('LspAttach', {
-      callback = function(args)
-        local opts = { buffer = args.buf }
-        
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)              -- Documentation
-        opts.desc = "Go to definition"
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)        -- Go to definition
-        opts.desc = "Refactor"
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)    -- Refactor/Rename
-        opts.desc = "Quick fix"
-        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts) -- Quick fixes
-      end,
-    })
+			-- Neovim 0.11 disables inline diagnostic text by default
+			vim.diagnostic.config({ virtual_text = true })
 
-    end,
-  },
+			-- Global Diagnostic Mappings
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local opts = { buffer = args.buf }
+
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) -- Documentation
+					opts.desc = "Go to definition"
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- Go to definition
+					opts.desc = "Refactor"
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- Refactor/Rename
+					opts.desc = "Quick fix"
+					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts) -- Quick fixes
+				end,
+			})
+		end,
+	},
 }
